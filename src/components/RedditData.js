a4b8144850398b16df1df9cca285a73e851683ea
Borce Lazarevski <94195865+BoLazDev@users.noreply.github.com>
2023-05-24 01:00:48 +0200

    Update RedditData.js

--- src/components/RedditData.js
+++ src/components/RedditData.js
@@ -14,9 +14,13 @@ const RedditData = ({ redditData }) => {
             {redditData && redditData.length !== 0 && (
                 redditData.map((el, i) => {
                     console.log(el);
-                    const { author, url, title, selftext, thumbnail } = el.data;
+                    const { author, url, title, selftext, thumbnail, preview } = el.data;
                     const isExpanded = expanded[i];
                     const truncatedText = selftext.slice(0, 200);
+                    // Check if thumbnail or preview image is available
+                    const imageSource = thumbnail && thumbnail !== 'self' && thumbnail !== null
+                    ? thumbnail
+                    : (preview && preview.images && preview.images[0] && preview.images[0].source && preview.images[0].source.url);
                     return (
                         <div className='col-md-4 pt-3 pb-3' key={i}>
                             <div className='card_reddit'>
@@ -35,8 +39,8 @@ const RedditData = ({ redditData }) => {
                                 )}
                                 <h6 className='author'>Author: {author}</h6>
                                 <a href={url} target='_blank' className='btn-details'>More Details</a>
-                                {thumbnail && thumbnail !== 'self' && thumbnail !== null && (
-                                    <img className='img-fluid' alt='reddit-thumbnail' src={thumbnail} />
+                                {imageSource && (
+                                    <img className='img-fluid' alt='reddit-thumbnail' src={imageSource} />
                                 )}
                             </div>
                         </div>
